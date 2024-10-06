e4b0b600f71550686bfa08fcaa69ff31fa83bd8a
melrwa <orwamelkizedek@gmail.com>
2024-10-06 16:17:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+// for(let age=30; age<40; age++){
+//     console.log(`I am ${age} happy birthday to me`)
+   
+// }
+// const gifts = ["teddy bear", "drone", "doll"];
+// function wrapGifts(gifts){
+// for(let i=0; i<gifts.length; i++){
+//     console.log(`wrapped ${gifts[i]} and added a bow`);
+//     debugger;
+// }
+// return gifts;
+// }
+// wrapGifts(gifts);
+let namesArray=["Charlie", "Samip", "Ali"];
+
+function writeCards(namesArray, event){
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+    let message = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+    
+    thankYouMessages.push(message);
+
+ }
+ return thankYouMessages
+
+
+}
+let messages=writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(messages);
+function countDown(number){
+    for(let i=number;i>=0; i--)
+        console.log(i)
+    
+
+}
+countDown(5)
+
